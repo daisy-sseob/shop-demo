1d77ac1fdad0bd112a2e69dbab9b11feb41d7d53
hyun-sseob <orolsyeo@gmail.com>
2022-08-16 21:06:58 +0900

    Entity 설계 및 Database connection 정보 설정

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,37 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "item_id")
+	private Long id; // 상품 코드
+
+	@Column(nullable = false, length = 50)
+	private String itemNm; // 상품 명
+	
+	@Column(nullable = false)
+	private int price; // 가격
+	
+	@Column(nullable = false)
+	private int stockNumber; // 재고수량
+
+	@Lob
+	@Column(nullable = false)
+	private String itemDetail; // 상품 상세 설명
+
+	@Enumerated(EnumType.STRING)
+	private ItemSellStatus itemSellStatus; // 상품 판매 상태
+	
+	private LocalDateTime regTime; // 등록 시간
+	private LocalDateTime updateTime; // 수정 시간
+}
