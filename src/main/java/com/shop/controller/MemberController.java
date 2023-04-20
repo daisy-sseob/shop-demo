49dbc55aee8d5d078ccce5db2264868f86ec732c
hyun-sseob <orolsyeo@gmail.com>
2023-04-20 17:38:59 +0900

    feat: commit

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -36,7 +36,7 @@ public class MemberController {
 		model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해 주세요.");
 		return "/member/memberLoginForm";
 	}
-
+	
 	@GetMapping("/new")
 	public String memberForm(Model model) {
 		model.addAttribute("memberFormDto", new MemberFormDto());
